08a3ee2afa4f567dff174bd0240caa0a7c552c81
ihor <ihoryaremko@gmail.com>
2019-01-28 18:00:14 +0200

    initial commit and start project

---
+++ back/app.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const feedRoutes = require('./routes/feed');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, res, cb) => {
+    if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+app.use(bodyParser.json());
+app.use(
+    multer({storage: fileStorage, fileFilter: fileFilter}).single('image')
+);
+app.use('/image', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    );
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+
+    const status = error.statusCode || 500;
+    const message = error.message;
+
+    res.status(status).json({
+        message: message
+    });
+});
+
+mongoose
+    .connect(
+        'mongoose connect will be here'
+    )
+    .then(resule => {
+        app.listen(8080);
+    })
+    .catch(error => console.log(error));
---
+++ back/controllers/feed.js
@@ -0,0 +1,89 @@
+const { validationResult } = require('express-validator/check');
+
+const Post = require('../models/post');
+
+exports.getPosts = (req, res, next) => {
+    Post.find()
+        .then(posts => {
+            res
+                .status(200)
+                .json({
+                    message: 'Fetched posts successfully.',
+                    posts: posts
+                })
+        })
+        .catch(error => {
+            if (!error.statusCode) {
+                error.statusCode = 500;
+            }
+        });
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post!');
+                error.statusCode = 404;
+                throw error;
+            }
+
+            res.status(200).json({
+                message: 'Post fetched',
+                post: post
+            })
+        })
+        .catch(error => {
+            if (!error.statusCode) {
+                error.statusCode = 500;
+            }
+
+            next(error);
+        });
+};
+
+exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    if (!req.file) {
+        const error = new Error('No image provided.');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    const imageUrl = req.file.path;
+    const title = req.body.title;
+    const content = req.body.content;
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: imageUrl,
+        creator: {
+            name: 'Ihor Yaremko'
+        }
+    });
+
+    post
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Post created successfully!',
+                post: result
+            });
+        })
+        .catch(error => {
+            if (!error.statusCode) {
+                error.statusCode = 500;
+            }
+
+            next(error);
+        });
+};
